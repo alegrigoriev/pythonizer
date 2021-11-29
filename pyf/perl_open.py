7d46572252c82a8ca229372b6f87993f18fb7ebb
Joe Cool <snoopyjc@gmail.com>
2021-11-29 01:13:35 -0500

    0.912  2021/11/29  SNOOPYJC  Fix coding error in perl_open (encoding)

--- pyf/perl_open.py
+++ pyf/perl_open.py
@@ -3,7 +3,7 @@ def perl_open(file,mode,encoding=None):
     """Replacement for perl built-in open function when used in an expression.
        FIXME: Handle pipe I/O via subprocess"""
     try:
-        return open(file,mode,encoding)
+        return open(file,mode,encoding=encoding)
     except Exception:
         return None    # sys.last_value will be set
 
