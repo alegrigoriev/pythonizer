50bfbf62c10ef26277497a9d6f41775c5d0e4fb4
Joe Cool <snoopyjc@gmail.com>
2022-09-07 23:52:29 -0400

    0.983  2022/09/04  SNOOPYJC Additional fix for issue s87 with variable interpolation

--- Config/__init__.py
+++ Config/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Joe Cool"""
 ___email__ = 'snoopyjc@gmail.com'
-__version__ = '0.982'
+__version__ = '0.983'
 
 import perllib
 import subprocess
--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/snoopyjc/pythonizer',
-    version='0.982',
+    version='0.983',
     zip_safe=False,
 )
