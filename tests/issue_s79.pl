5e6f4b9238d6afb4ed5765492d5953fb13cd6ff7
Joe Cool <snoopyjc@gmail.com>
2022-10-26 17:43:21 -0400

    1.007  2022/10/24  SNOOPYJC issue s79: Use of uninitialized value  in rindex at ../pythonizer/pythonizer line, more fixes for issue s102

--- tests/issue_s79.pl
+++ tests/issue_s79.pl
@@ -13,6 +13,17 @@ testit();
 
 assert($_ eq '43andthat');
 
+sub testit_my
+{
+	$_ = 'thisandthat';
+	my $that = 41;
+	s/this/$that+1/e;
+}
+
+testit_my();
+
+assert($_ eq '42andthat');
+
 my %char2entity = ('&'=>'&amp;', '>'=>'&gt;', '<'=>'&lt;', '"'=> '&quot;', "'"=> '&apos;');
 
 sub return_or
@@ -21,15 +32,6 @@ sub return_or
 }
 assert(return_or('&') eq '&amp;');
 assert(return_or('+') eq '&#x2B;');
-
-sub return_or_split
-{
-	$char2entity{$_[0]} 
-	|| 
-	num_entity($_[0]);
-}
-assert(return_or_split('&') eq '&amp;');
-assert(return_or_split('+') eq '&#x2B;');
 
 sub return_and
 {
@@ -67,7 +69,54 @@ sub num_entity {
 my $entity = "abc&<def";
 my $encoded = encode_entities($entity);
 
-print "$encoded\n";
+#print "$encoded\n";
+assert($encoded eq 'abc&amp;&lt;def');
 
+sub return_conditional
+{
+    my $result;
+
+    $result = 1 if($_[0] == 1);
+}
+assert(return_conditional(1) == 1);
+assert(!return_conditional(0));
+
+sub return_conditional2
+{
+    return_conditional($_[0]) if($_[0] == 1);
+}
+assert(return_conditional2(1) == 1);
+assert(!return_conditional2(0));
+
+sub loop_exit
+{
+    my $cnt = $_[0];
+    $tot = 0;
+    $tot++ while($cnt--);
+}
+
+loop_exit(3);
+assert($tot == 3);      # Make sure we don't sneak in a 'return' in the loop
+
+sub process_file
+{
+    undef $fh;
+    close($fh) or die("cannot close file!");
+}
+eval {
+    process_file();
+};
+assert($@ =~ /close/);
+
+sub process_file2
+{
+    open(IN, "<$0");
+    undef $fh;
+    close(IN, $fh) or die("cannot close file!");
+}
+eval {
+    process_file();
+};
+assert($@ =~ /close/);
 	
 print "$0 - test passed!\n";
